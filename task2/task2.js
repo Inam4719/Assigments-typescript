f87c0ecdbd4d1c80c8ab68025ec2c7b56e82963e
Inam4719 <enamkhan.2016@gmail.com>
2023-08-17 20:24:07 +0500

    Task2 completed

---
+++ task2/task2.js
@@ -0,0 +1,4 @@
+"use strict";
+const myName = "inam";
+const message = `Hello ${myName} ,Would you like to learn some Python today?`;
+console.log(message);
---
+++ task2/task2.ts
@@ -0,0 +1,3 @@
+const myName ="inam"
+const message=`Hello ${myName} ,Would you like to learn some Python today?`
+console.log(message);
