b1a5df0d5dc198f62105b5bc1ebc27a63f8780be
Inam4719 <enamkhan.2016@gmail.com>
2023-08-17 20:55:38 +0500

    task3 done

---
+++ task3/task3.ts
@@ -0,0 +1,13 @@
+const myName="Inam Khan";
+const lowerCase=myName.toLowerCase();
+const upperCase=myName.toUpperCase();
+function toTitleCase(str:string): string {
+    return str.replace(/\w\S*/g, (txt) => {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+const titleName = toTitleCase(myName);
+console.log("Original name:",myName);
+console.log("Lowercase:",lowerCase);
+console.log("uppercase name:",upperCase);
+console.log("Title case name:",titleName);
