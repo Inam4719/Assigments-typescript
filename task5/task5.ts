7f13595f29c7194cca4d7ac744c7ed280b57d07b
Inam4719 <enamkhan.2016@gmail.com>
2023-08-20 10:54:00 +0500

    task 5 done

---
+++ task5/task5.ts
@@ -0,0 +1,4 @@
+const famous_person="Albert Einstein";
+const qoute="A person who never made a mistake never tried anything new";
+const message=`${famous_person} once said,"${qoute}"`;
+console.log(message);
