86610952880bb8f2511d8f2627a92a84aac9703d
Inam4719 <enamkhan.2016@gmail.com>
2023-08-20 11:07:11 +0500

    task 6 done

---
+++ task6/task6.js
@@ -0,0 +1,5 @@
+"use strict";
+const nameSpace = "\t  \n John Doe \t\n";
+console.log("Name with whitespace:", nameSpace);
+const strippedName = nameSpace.trim();
+console.log("\nName after stripping whitespace:", strippedName);
---
+++ task6/task6.ts
@@ -0,0 +1,4 @@
+const nameSpace = "\t  \n John Doe \t\n";
+console.log("Name with whitespace:",nameSpace);
+const strippedName = nameSpace.trim();
+console.log("\nName after stripping whitespace:",strippedName);
