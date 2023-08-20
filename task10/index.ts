0ee590e52ec68529a921fe8263f58831a96c85e1
Inam4719 <enamkhan.2016@gmail.com>
2023-08-20 22:43:13 +0500

    task 10 done

---
+++ task10/index.ts
@@ -0,0 +1,7 @@
+console.log(5 + 3);   // Addition: 5 + 3 = 8
+console.log(10 - 2);  // Subtraction: 10 - 2 = 8
+console.log(4 * 2);   // Multiplication: 4 * 2 = 8
+console.log(16 / 2);  // Division: 16 / 2 = 8
+const favoriteNumber = 42;
+const message = `My favorite number is ${favoriteNumber}.`;
+console.log(message);
