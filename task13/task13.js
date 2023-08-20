f34139f30886dac9807ff2b81f7ae4aa320278cf
Inam4719 <enamkhan.2016@gmail.com>
2023-08-20 23:08:40 +0500

    task13 done

---
+++ task13/task13.js
@@ -0,0 +1,4 @@
+"use strict";
+const transport = ["Car", "Motarcycle", "Bus", "cycle"];
+console.log(`i would like to my own transport ${transport[0]}.`);
+console.log(`Riding a ${transport[1]} is a great way to stay healthy.`);
---
+++ task13/task13.ts
@@ -0,0 +1,3 @@
+const transport=["Car","Motarcycle","Bus","cycle"]
+console.log(`i would like to my own transport ${transport[0]}.`);
+console.log(`Riding a ${transport[1]} is a great way to stay healthy.`)
