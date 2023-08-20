a06f8ada86cc7b31a9d96f85f791e86816f116a4
Inam4719 <enamkhan.2016@gmail.com>
2023-08-20 22:52:18 +0500

    task11 done

---
+++ task11/task11.js
@@ -0,0 +1,6 @@
+"use strict";
+const names = ["Inam", "Ali", "Asim", "asif"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
---
+++ task11/task11.ts
@@ -0,0 +1,5 @@
+const names:string[]=["Inam","Ali","Asim","asif"]
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
