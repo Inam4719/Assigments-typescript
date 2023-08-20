b8df884f0c7bd605daf68ddbf37b299f64e983b2
Inam4719 <enamkhan.2016@gmail.com>
2023-08-20 22:29:09 +0500

    task 7 done

---
+++ task7/index.ts
@@ -0,0 +1,8 @@
+const addition=5+3;
+console.log(`addition 5+3=${addition}`);
+const subtact=11-3;
+console.log(`subtraction 11-3=${subtact}`);
+const multiply=4*2;
+console.log(`Multiplication 4*2=${multiply}`);
+const division=24/3;
+console.log(`Division 24/3= ${division}`);
