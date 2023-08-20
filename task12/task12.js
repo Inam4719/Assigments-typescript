13e1b145d9fb3fa66404352abc4e520ed95c7f63
Inam4719 <enamkhan.2016@gmail.com>
2023-08-20 22:58:21 +0500

    task12 done

---
+++ task12/task12.js
@@ -0,0 +1,7 @@
+"use strict";
+const names = ["Inam", "Ali", "Asim", "asif"];
+const greeting = "Hello";
+console.log(greeting + names[0]);
+console.log(greeting + names[1]);
+console.log(greeting + names[2]);
+console.log(greeting + names[3]);
---
+++ task12/task12.ts
@@ -0,0 +1,6 @@
+const names:string[]=["Inam","Ali","Asim","asif"];
+const greeting="Hello"
+console.log(greeting+ names[0]);
+console.log(greeting+ names[1]);
+console.log(greeting+ names[2]);
+console.log(greeting+ names[3]);
