49313ea426cb3dffc1a0bc97774c02c81236756f
Inam4719 <enamkhan.2016@gmail.com>
2023-08-21 20:20:32 +0500

    task22 done

---
+++ task22/task22.js
@@ -0,0 +1,10 @@
+"use strict";
+const myArray = [1, 2, 3, 4, 5];
+const indexToAccess = 10;
+console.log(myArray[indexToAccess]);
+if (indexToAccess >= 0 && indexToAccess < myArray.length) {
+    console.log(myArray[indexToAccess]);
+}
+else {
+    console.log("Index is out of bounds");
+}
