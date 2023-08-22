91d1d421d290d5e1b98fdf527eb4c6622b5caf66
Inam4719 <enamkhan.2016@gmail.com>
2023-08-22 11:32:58 +0500

    task25 done

---
+++ task25/task25.js
@@ -0,0 +1,9 @@
+"use strict";
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Player just earned 5 points!");
+}
+var alien_color = 'yellow';
+if (alien_color === 'green') {
+    console.log("player just earned 5 points.");
+}
