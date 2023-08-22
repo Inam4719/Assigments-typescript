0d97bcc5e680c71693389645c2227f3d47fb4311
Inam4719 <enamkhan.2016@gmail.com>
2023-08-22 12:11:48 +0500

    task32 done

---
+++ task32/task32.js
@@ -0,0 +1,18 @@
+"use strict";
+var current_users = ['admin', 'Eric', 'Alice', 'John', 'Megan'];
+var new_users = ['Alice', 'Bob', 'John', 'David', 'Jane'];
+for (var i = 0; i < new_users.length; i++) {
+    var isTaken = false;
+    for (var j = 0; j < current_users.length; j++) {
+        if (new_users[i].toLowerCase() === current_users[j].toLowerCase()) {
+            isTaken = true;
+            break;
+        }
+    }
+    if (isTaken) {
+        console.log("Sorry, the username '" + new_users[i] + "' is already taken. Please enter a new username.");
+    }
+    else {
+        console.log("The username '" + new_users[i] + "' is available.");
+    }
+}
