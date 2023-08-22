03ab895b93199e3674d7dd5af5f63147e5a595bc
Inam4719 <enamkhan.2016@gmail.com>
2023-08-22 11:52:34 +0500

    task29 done

---
+++ task29/task29.ts
@@ -0,0 +1,19 @@
+var favorite_fruits=["banna","apple","mangoes"];
+if(favorite_fruits.includes('banna')){
+    console.log("you really like banna")
+}
+if (favorite_fruits.includes('apple')) {
+    console.log("You really like apples!");
+}
+
+if (favorite_fruits.includes('mangoes')) {
+    console.log("You really like mangoes!");
+}
+
+if (favorite_fruits.includes('orange')) {
+    console.log("You really like oranges!");
+}
+
+if (favorite_fruits.includes('kiwi')) {
+    console.log("You really like kiwis!");
+}
